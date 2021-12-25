27da1d1133715e8b8e9f0986f514004b7797e7bb
Nguyễn Huy Hoàng <94150981+TylerHoangNg@users.noreply.github.com>
2021-12-25 07:46:55 +0700

    Update

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -25,7 +25,7 @@
 		<div class="row">
 			<div class="col-12">
 				<nav class="main-nav">
-					<a href="index.html" class="logo">
+					<a href="/" class="logo">
 						<img src={logo} alt="Horizan App" />
 					</a>
 					<span
@@ -39,7 +39,7 @@
 						<span aria-hidden="true" />
 					</span>
 					<!-- ***** Menu ***** -->
-					<!-- <ul class="nav">
+					<ul class="nav">
 						<li class="scroll-to-section">
 						  <a class="button" class:active={$page.path === '/'} sveltekit:prefetch href="/"><i class="fas fa-home"/>Home</a>
 						</li>
@@ -54,7 +54,7 @@
 							  <a class="buton is-primary-is-light" href="/"> <i class="fa fa-sign-out" aria-hidden="true"></i> Login <i class="fas fa-sign-in-alt"/></a>
 						  </div>
 						</li>
-					</ul> -->
+					</ul>
 				</nav>
 			</div>
 		</div>
